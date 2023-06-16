627926e7010bc5a680b50d845b3a892f2a384867
Raphael Kim <rageworx@gmail.com>
2023-06-16 15:14:26 +0900

    version reset to 0.0.0.1

--- res/resource.h
+++ res/resource.h
@@ -1,3 +1,3 @@
-#define APP_VERSION								0,4,8,94
-#define APP_VERSION_STR							"0.4.8.94"
+#define APP_VERSION								0,0,0,1
+#define APP_VERSION_STR							"0.0.0.1"
 #define IDC_ICON_A								101
