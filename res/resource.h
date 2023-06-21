36ee5f643acfb5d136d487d79d9935c9f2936311
Raphael Kim <rageworx@gmail.com>
2023-06-21 16:19:49 +0900

    removed tab charactors in res/resource.h

--- res/resource.h
+++ res/resource.h
@@ -1,3 +1,3 @@
-#define APP_VERSION								0,2,0,15
-#define APP_VERSION_STR							"0.2.0.15"
-#define IDC_ICON_A								101
+#define APP_VERSION                             0,2,0,15
+#define APP_VERSION_STR                         "0.2.0.15"
+#define IDC_ICON_A                              101
