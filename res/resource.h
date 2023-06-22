fa519a6f1f4ca002fe73493dfb413ebb6dd15c27
Raphael Kim <rageworx@gmail.com>
2023-06-22 15:28:14 +0900

    version (for GUI) updated to 0.2.3.20.

--- res/resource.h
+++ res/resource.h
@@ -1,3 +1,3 @@
-#define APP_VERSION                             0,2,0,15
-#define APP_VERSION_STR                         "0.2.0.15"
+#define APP_VERSION                             0,2,3,20
+#define APP_VERSION_STR                         "0.2.3.20"
 #define IDC_ICON_A                              101
