6db52b724c7004ceaa4f499d3b3aa444dbf1783f
Raphael Kim <rageworx@gmail.com>
2023-07-13 17:47:08 +0900

    version tagging to 0.3.0.41

--- res/resource.h
+++ res/resource.h
@@ -1,3 +1,3 @@
-#define APP_VERSION                             0,2,3,20
-#define APP_VERSION_STR                         "0.2.3.20"
+#define APP_VERSION                             0,3,0,41
+#define APP_VERSION_STR                         "0.3.0.41"
 #define IDC_ICON_A                              101
